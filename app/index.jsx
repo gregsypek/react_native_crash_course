af3b67b42be1dab23a75d8e23e92db2da7dfb4d4
gregsypek <47268138+gregsypek@users.noreply.github.com>
2024-07-17 09:41:41 +0200

    add home page

--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,50 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 
 import { NativeWindStyleSheet } from "nativewind";
-
-NativeWindStyleSheet.setOutput({
-	default: "native",
-});
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
 	return (
-		<View className="flex-1 items-center justify-center bg-blue-300">
-			<Text className="text-3xl font-pblack">Aurora</Text>
-			<StatusBar style="auto" />
-			<Link href="/profile" style={{ color: "blue" }}>
-				Go t Profile
-			</Link>
-		</View>
+		<SafeAreaView className="bg-primary h-full">
+			<ScrollView contentContainerStyle={{ height: "100%" }}>
+				<View className="w-full justify-center items-center h-full px-4">
+					<Image
+						source={images.logo}
+						className="w-[130px] h-[84px]"
+						resizeMode="contain"
+					/>
+					<Image
+						source={images.cards}
+						className="max-w-[380px] w-full h-[300px]"
+						resizeMode="contain"
+					/>
+					<View className="relative mt-5">
+						<Text className="text-3xl text-white font-bold text-center">
+							Discover Endless Possibilites{" "}
+							<Text className="text-secondary-200">Aora</Text>
+						</Text>
+						<Image
+							source={images.path}
+							className="w-[136px] h-[15px] absolute-bottom-2 -right-8"
+						/>
+					</View>
+					<Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+						Where creativity meets innovation: emark on a journey of limitless
+						exploration with Aora
+					</Text>
+					<CustomButton
+						title="Continue with Email"
+						handlePress={() => {}}
+						containerStyles="w-full mt-7"
+					/>
+				</View>
+				<StatusBar />
+			</ScrollView>
+			<StatusBar backgroundColor="#161622" style="light" />
+		</SafeAreaView>
 	);
 }
